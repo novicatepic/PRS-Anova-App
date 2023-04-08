93274b47c991f4e60e818247b38b93d5ab6b6fe7
DESKTOP-SV2RG4H\Тепица <acivon@outlook.com>
2023-04-08 21:31:56 +0200

    Gotovo?

--- src/org/unibl/etf/lab3/InputParameters.java
+++ src/org/unibl/etf/lab3/InputParameters.java
@@ -11,6 +11,8 @@ import javax.swing.SwingConstants;
 import java.awt.GridLayout;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.awt.event.ActionEvent;
 
 public class InputParameters extends JFrame {
@@ -72,6 +74,7 @@ public class InputParameters extends JFrame {
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				calculateParameters();
+				
 			}
 		});
 		btnNewButton.setBounds(504, 43, 85, 21);
@@ -80,9 +83,13 @@ public class InputParameters extends JFrame {
 		System.out.println(numMeasurements);*/
 	}
 	
+	private double contrastValue;
+	private double fValue;
 	public void setParameters(int numAlternatives, int numMeasurements, double contrastValue, double fValue) {
 		this.numAlternatives = numAlternatives;
 		this.numMeasurements = numMeasurements;
+		this.contrastValue = contrastValue;
+		this.fValue = fValue;
 		panel.setLayout(new GridLayout(numMeasurements, numAlternatives));
 		
 		textFields = new JTextField[numMeasurements][numAlternatives];
@@ -122,6 +129,7 @@ public class InputParameters extends JFrame {
 	private double meanSquareAlternatives;
 	private double meanSquareErrors;
 	private double computedF;
+	private boolean doesDifferenceExist = false;
 	
 	private void calculateParameters() {
 		double[] sumOfAlternatives = new double[numAlternatives];
@@ -192,6 +200,12 @@ public class InputParameters extends JFrame {
 		System.out.println("Mean square errors = " + meanSquareErrors);
 		System.out.println("Computed F = " + computedF);
 		
+		if(computedF > fValue) {
+			doesDifferenceExist = true;
+		}
+		
+		System.out.println("Difference exists = " + doesDifferenceExist);
+		
 		for(int i = 0; i < numAlternatives; i++) {
 			for(int j = 0; j < numAlternatives; j++) {
 				if(i < j) {
@@ -200,18 +214,33 @@ public class InputParameters extends JFrame {
 			}
 		}
 		
+		ShowResults showResults = new ShowResults(meanSquareColumns, effects, meanSquare, SSA, SSE, SST, degFreedomAlternatives, degFreedomErrors,
+				degFreedomTotal, meanSquareAlternatives, meanSquareErrors, computedF, fValue);
+		showResults.setVisible(true);
+		ShowProbabilities showProbabilities = new ShowProbabilities(map);
+		showProbabilities.setVisible(true);
 	}
 	
-	private void calculateContrast(int alternative1, int alternative2) {
-		int weight1 = 1, weight2 = -1;
-		
+	private ArrayList<String> map = new ArrayList<>();
+	private void calculateContrast(int alternative1, int alternative2) {	
 		double sc = 0.0, scTemp = 0.0;
 		scTemp += 2.0 / Double.valueOf(numAlternatives * numMeasurements);
-		sc = Math.sqrt(scTemp) * meanSquareErrors;
+		sc = Math.sqrt(scTemp) * Math.sqrt(meanSquareErrors);
 		
 		double effect1 = effects[alternative1];
 		double effect2 = effects[alternative2];
 		double c = effect1 - effect2;
+		
+		double c1 = c - contrastValue * sc;
+		double c2 = c + contrastValue * sc;
+		String toMap = ("("+String.valueOf(alternative1+1)+", "+String.valueOf(alternative2+1)+") = " + 
+		"(" + String.valueOf(c1) + ", " + String.valueOf(c2) + ")");
+		//map.put(toMap, null)
+		map.add(toMap);
+		System.out.println("Between alternatives: " + alternative1 + " and " + alternative2);
+		System.out.println("C1 = " + c1);
+		System.out.println("C2 = " + c2);
+		
 		
 	}
 }
